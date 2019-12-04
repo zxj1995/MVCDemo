4b679648c5b1a5ad684dd0a841b5e0dafc01c840
zxj1995 <1157252369@qq.com>
2019-12-04 14:42:51 +0800

    项目初始化

---
+++ MVCDemo/MVCDemo/Controllers/StudentListController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDemo.Controllers
+{
+    public class StudentListController : Controller
+    {
+        // GET: StudentList
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
