5a07cfc8f912c655baa2dae52055b50688a316c6
zxj1995 <1157252369@qq.com>
2019-12-04 16:59:39 +0800

    增删查改基本框架

---
+++ MVCDemo/MVCDemo/MVCDemo/Controllers/StuListController.cs
@@ -0,0 +1,56 @@
+﻿using MVCDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDemo.Controllers
+{
+    public class StuListController : Controller
+    {
+        // GET: StuList
+        StudentLoginEntities dbcontent = new StudentLoginEntities();
+        public ActionResult Index()
+        {
+            ViewData.Model = dbcontent.StudentTable.AsEnumerable();
+            return View();
+        }
+
+
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(StudentTable stu)
+        {
+            ViewData.Model = dbcontent.StudentTable.AsEnumerable();
+            return View();
+        }
+
+        public ActionResult Delete(int id)
+        {
+            ViewData.Model = dbcontent.StudentTable.Where(u => u.id == id).FirstOrDefault();
+            return View();
+        }
+        public ActionResult Edit(int id)
+        {
+            ViewData.Model = dbcontent.StudentTable.Where(u => u.id == id).FirstOrDefault();
+            return View();
+        }
+        public ActionResult Details(int id)
+        {
+            ViewData.Model = dbcontent.StudentTable.Where(u => u.id == id).FirstOrDefault();
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Delete(int id,FormCollection fc)
+        {
+            ViewData.Model = dbcontent.StudentTable.Find(id);
+            return View();
+        }
+
+    }
+}
